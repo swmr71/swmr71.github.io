d766f522b1e89af24ed23db0e6d08ed6211986f3
swmr71 <swmr71@icloud.com>
2026-05-10 20:33:19 +0900

    Create installalloy.sh for Alloy installation
    Install necessary tools, add GPG key and repository for Alloy.

---
+++ installalloy.sh
@@ -0,0 +1,14 @@
+# 必要なツールのインストール
+sudo apt-get update
+sudo apt-get install -y apt-transport-https software-properties-common wget
+
+# GPGキーの登録
+sudo mkdir -p /etc/apt/keyrings
+wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
+
+# リポジトリの追加
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee /etc/apt/etc/apt/sources.list.d/grafana.list
+
+# インストール
+sudo apt-get update
+sudo apt-get install alloy
