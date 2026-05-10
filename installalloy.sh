310128f5a641893fbfe33b158b644590d084d03a
swmr71 <swmr71@icloud.com>
2026-05-10 21:41:06 +0900

    Add files via upload

--- installalloy.sh
+++ installalloy.sh
@@ -7,7 +7,7 @@ sudo mkdir -p /etc/apt/keyrings
 wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
 
 # リポジトリの追加
-echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee /etc/apt/etc/apt/sources.list.d/grafana.list
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
 
 # インストール
 sudo apt-get update
